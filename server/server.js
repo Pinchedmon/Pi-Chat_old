2ae3b07ac60578cae53335ea83eba6298510c211
Alexey <temnikovalx@icloud.com>
2022-07-22 19:15:22 +0300

    to future improvements

--- server/server.js
+++ server/server.js
@@ -18,13 +18,13 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 app.use('/auth', authRouter)
 //убрать
-app.use(express.static(path.join(__dirname, '/../client/build')));
+//app.use(express.static(path.join(__dirname, '/../client/build')));
 
 //console.log(path.join(__dirname, 'build'))
 //Убрать
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/../client', 'build', 'index.html'));
-});
+//app.get('/*', (req, res) => {
+//  res.sendFile(path.join(__dirname, '/../client', 'build', 'index.html'));
+//});
 const DIR = './public';
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
